605233f397a9efda518506645689bb33bc3143dd
Bhoomi Shah <thisisbhoomishah@gmail.com>
2021-02-04 19:01:32 +0530

    Updated testcase

--- src/test/java/com/calculator/calculator/AppTest.java
+++ src/test/java/com/calculator/calculator/AppTest.java
@@ -34,6 +34,6 @@ public class AppTest extends TestCase
 		a.a=5;
         a.b=10;
         assertEquals(15,a.add());
-        assertEquals(-5,a.subtract());
+        assertEquals(15,a.subtract());
 	}
 }
